658d20c3ca404ad06a769e7e7f9d4d1a1ab1fbe5
skynesser <3250532948@qq.com>
2022-05-13 15:28:19 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   src/component/editorPane.java
    	modified:   src/test/Test2.java

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -1,18 +1,7 @@
 package test;
 
-import javax.swing.*;
-
 public class Test2 {
-    public static void main(String[] args) {
-        JFrame jFrame = new JFrame();
-
-        jFrame.setLayout(null);
-
-        jFrame.setBounds(0,0,1000,600);
-
-
-
-
+    public static void main(String[] args) throws InterruptedException {
 
     }
 }
